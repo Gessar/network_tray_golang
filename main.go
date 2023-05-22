500efe370ca02a1bb57b74501422792f19de8967
Gessar <Gessar2010@gmail.com>
2023-05-22 17:29:28 +0600

    kb to mb > 1024

--- main.go
+++ main.go
@@ -55,9 +55,10 @@ func onReady() {
 			}
 
 			if lastBytesSent != 0 && lastBytesRecv != 0 {
-				currentUploadSpeed := strconv.FormatFloat(float64(currentBytesSent-lastBytesSent)/1024.0, 'f', 2, 64)
-				currentDownloadSpeed := strconv.FormatFloat(float64(currentBytesRecv-lastBytesRecv)/1024.0, 'f', 2, 64)
-				speed := "Upload speed: " + currentUploadSpeed + " KB/s Download speed: " + currentDownloadSpeed + " KB/s"
+				//currentUploadSpeed := strconv.FormatFloat(float64(currentBytesSent-lastBytesSent)/1024.0, 'f', 2, 64)
+				//currentDownloadSpeed := strconv.FormatFloat(float64(currentBytesRecv-lastBytesRecv)/1024.0, 'f', 2, 64)
+				//speed := "Upload speed: " + currentUploadSpeed + " KB/s Download speed: " + currentDownloadSpeed + " KB/s"
+				speed := formatSpeedText(currentBytesSent, lastBytesSent, currentBytesRecv, lastBytesRecv)
 				fmt.Printf("\r%s", speed)
 				downloadSpeed <- speed
 			}
@@ -75,4 +76,19 @@ func onReady() {
 
 func onExit() {
 	fmt.Println("\nExiting...")
+}
+
+func formatSpeedText(currentBytesSent uint64, lastBytesSent uint64, currentBytesRecv uint64, lastBytesRecv uint64) string {
+	uploadString, downloadString := "KB/s", "KB/s"
+	currentUploadSpeed := strconv.FormatFloat(float64(currentBytesSent-lastBytesSent)/1024.0, 'f', 2, 64)
+	currentDownloadSpeed := strconv.FormatFloat(float64(currentBytesRecv-lastBytesRecv)/1024.0, 'f', 2, 64)
+	if float64(currentBytesSent-lastBytesSent)/1024.0 >= 1024 {
+		currentUploadSpeed = strconv.FormatFloat(float64(currentBytesSent-lastBytesSent)/1048576.0, 'f', 2, 64)
+		uploadString = "MB/s"
+	}
+	if float64(currentBytesRecv-lastBytesRecv)/1024.0 >= 1024 {
+		currentDownloadSpeed = strconv.FormatFloat(float64(currentBytesRecv-lastBytesRecv)/1048576.0, 'f', 2, 64)
+		downloadString = "MB/s"
+	}
+	return "Upload speed: " + currentUploadSpeed + " " + uploadString + " Download speed: " + currentDownloadSpeed + " " + downloadString
 }
